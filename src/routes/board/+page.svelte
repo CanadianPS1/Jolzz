9c69b78adfceb855c6b104da815f2cf0c2cab476
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-11-26 19:48:27 -0700

    Moved assets folder, created spawing system for the pieces, implemented the spawing of the black side pieces into random possitions

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -1,14 +1,65 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { tiles, row, columns} from '../../services/Board.ts';
+    import { pieces, row, columns} from '../../services/Board.ts';
+    var whitePiecesToSpawn : number = 16;
+    var blackPiecesToSpawn : number = 16;
+    var tileCount : number = 0;
+    for(var i = pieces.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [pieces[i], pieces[j]] = [pieces[j], pieces[i]];
+    }
     onMount(() => {
     row.forEach(row => {
+        tileCount++;
         columns.forEach(col => {
         const id = `${col}${row}`;
         const button = document.getElementById(id) as HTMLButtonElement | null;
         if(button){
             button.addEventListener('click', () => console.log('Clicked ' + id));
             button.disabled = true;
+            const amountOfPieces : number = 32 - (whitePiecesToSpawn + blackPiecesToSpawn);
+            const chanceOfPiece : number = ((64 - tileCount) - amountOfPieces);
+            if(Math.floor(Math.random() * (100 - 1 + 1)) + 1 > chanceOfPiece && amountOfPieces <= 32){
+                //button.disabled = false;
+                if((16 - whitePiecesToSpawn) < (16 - blackPiecesToSpawn) && whitePiecesToSpawn > 0){
+                    whitePiecesToSpawn -= 1;
+                    console.log('spawned a white piece at : ' + col + row);
+                }else if((16 - whitePiecesToSpawn) > (16 - blackPiecesToSpawn) && blackPiecesToSpawn > 0){
+                    button.disabled = false;
+                    const pieceNumber = 16 - blackPiecesToSpawn;
+                    blackPiecesToSpawn -= 1;
+                    console.log('number of black pieces : ' + pieceNumber);
+                    const piece : string = pieces[pieceNumber];
+                    const imageURL = '/assets/BlackSidePieces/blackSide'+piece+'.png';
+                    button.style.backgroundImage = "url(" + imageURL + ")";
+                    button.style.backgroundSize = "cover";
+                    button.style.backgroundPosition = "center";
+                    button.textContent = piece;
+                    
+                    console.log('spawned a black piece at : ' + col + row);
+                }else{
+                    if(Math.floor(Math.floor(Math.random() * (100 - 1 + 1)) + 1) > 50 && whitePiecesToSpawn > 0){
+                        whitePiecesToSpawn -= 1;
+                        console.log('spawned a white piece at : ' + col + row);
+                    }else if(blackPiecesToSpawn > 0){
+                        button.disabled = false;
+                        const pieceNumber = 16 - blackPiecesToSpawn;
+                        blackPiecesToSpawn -= 1;
+                        console.log('number of black pieces : ' + pieceNumber);
+                        const piece : string = pieces[pieceNumber];
+                        const imageURL = '/assets/BlackSidePieces/blackSide'+piece+'.png';
+                        button.style.backgroundImage = "url(" + imageURL + ")";
+                        button.style.backgroundSize = "cover";
+                        button.style.backgroundPosition = "center";
+                        button.textContent = piece;
+                        
+                        console.log('spawned a black piece at : ' + col + row);
+                    }
+                }
+            }
+            console.log('Amount of Pieces: ' + amountOfPieces + 
+                        '\nAmount of Black Pieces :' + (16 - blackPiecesToSpawn) + 
+                        '\nAmount of White Pieces : ' + (16 - whitePiecesToSpawn));
         }
         });
     });
@@ -36,9 +87,7 @@
 			</div>
 		{/each}
 	</h1>
-    <h1 class="pieces">
-        pawn
-    </h1>
+    
 	<h2>
 		<div class="username">
 			<div class="u2WinsLosses">W/L</div>
--- src/services/Board.ts
+++ src/services/Board.ts
@@ -1,7 +1,7 @@
 export const row: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
 export const columns: string[] = ["a","b","c","d","e","f","g","h"];
 export const tiles: { [key: string]: HTMLButtonElement } = {};
-
+export const pieces: string[] = ["Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Bishup", "Bishup", "Knight", "Knight", "Rook", "Rook", "Queen", "King"];
 // document.addEventListener('DOMContentLoaded', () => {
 //   row.forEach(row => {
 //     columns.forEach(col => {
