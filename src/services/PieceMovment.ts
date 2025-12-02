961648c1e3a7101bef8fc887ce40c2baa1840536
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-12-02 16:01:52 -0700

    started the movment stuff

---
+++ src/services/PieceMovment.ts
@@ -0,0 +1,18 @@
+function pawnMovment(kingLocation : string, pawnLocation : string, pawnSide : string){
+
+}
+function rookMovment(){
+
+}
+function bishupMovment(){
+
+}
+function knightMovment(){
+
+}
+function queenMovment(){
+
+}
+function kingMovment(){
+
+}
